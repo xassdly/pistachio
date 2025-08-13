dd182768df5af67672516e8e4718f10c4339486f
xassdly <xassdly@gmail.com>
2025-08-13 16:42:20 +0200

    ready for vercel x8

--- app/api/user/me/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getCurrentUser } from '@/lib/getCurrentUser';
-
-export async function GET() {
-  const user = await getCurrentUser();
-  return NextResponse.json({ user });
-}
